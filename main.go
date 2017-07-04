5a3c424f3b9d375f1729dc200901f768a1de7579
belynda <belynda.hamaz@etu.univ-paris-diderot.fr>
2017-07-04 10:11:47 +0200

    se connecter

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 
 //const node string = "[fe80::e046:9aff:fe4e:912e%wlp1s0]:33123"
 
-const node string = "[fe80::e046:9aff:fe4e:912e%enp2s0]:33123"
+//const node string = "[fe80::e046:9aff:fe4e:912e%enp2s0]:33123"
 
 //const node string = "[fe80::1e8f:814e:9731:dec6%enp2s0]:33123"
 
-//const node string = "[::1]:33123"
+const node string = "[::1]:33123"
 
 func main() {
 
