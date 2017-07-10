14b474ae233a2a8d64a904a866e9ae2b8f4b78c5
belynda <belynda.hamaz@etu.univ-paris-diderot.fr>
2017-07-10 11:55:58 +0200

    static

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,52 +1,200 @@
+/* ----       BabelWebV2          ----*/
 
-/* ------------   Connexion au serveur et attente de message  ---------------- */
-function connect(){
+function BabelWebV2() {
+  /* ----    A propos de babel    ----*/
+
+  var nodes = [];
+  var links = [];
+
+  function Node(id ,group) {
+          this.id = id;
+          this.group = group;
+  }
+
+  function Link(source,target,value) {
+          this.source = source;
+          this.target = target;
+          this.value = value;
+  }
+
+  /*----   test   ----*/
+  nodes.push(new Node("Myriel",1));
+  nodes.push(new Node("Napoleon",1));
+  nodes.push(new Node("Mlle.Baptistine",1));
+  nodes.push(new Node("Mme.Magloire", 1));
+
+  links.push(new Link("Napoleon","Myriel",1));
+  links.push(new Link("Mlle.Baptistine","Myriel",8));
+  links.push(new Link("Mme.Magloire","Myriel",10));
+  links.push(new Link("Mme.Magloire", "Mlle.Baptistine", 6));
+
+
+  function connect(){
     var socket = null;
     try {
-        socket = new WebSocket("ws://localhost:8080/ws");
+      socket = new WebSocket("ws://localhost:8080/ws");
 
     } catch (exception) {
-        console.error(exception);
+      console.error(exception);
     }
 
     socket.onerror = function(error) {
-        console.error(error);
+      console.error(error);
     };
 
-    // Lorsque la connexion est établie.
     socket.onopen = function(event) {
-        console.log("Connected.");
-        var t = d3.select("body")
-          .append("p")
-          .text("Connected");
-
-        this.onclose = function(event) {
-        console.log("Disconnected.");
-        };
-
-        this.onmessage = function(event) {
-            console.log(event)
-            console.log(event.data);
-            var data = JSON.parse(event.data);
-            console.log(data);
-            var t = d3.select("body")
+      console.log("Connected.");
+    var t = d3.select("body")
               .append("p")
-              .text(data.Name);
+              .text("Connected");
+    }
 
-           // ConvertJSON(event);
-            // var t = d3.select("body")
-            //   .append("p")
-            //   .text("message");
-        };
-        // Envoi d'un message vers le serveur.
-        //this.send("Hello world!");
+    this.onclose = function(event) {
+      console.log("Disconnected.");
     };
+
+    this.onmessage = function(event) {
+      console.log(event)
+      ConvertJSON(event)
+    };
+
+    // this.send("Hello world!");
+  }
+
+  function init(){
+
+  }
+
+  function ConvertJSON(message) {
+    var data = JSON.parse(message.data);
+
+    switch (data.action) {
+      case "add": 
+        break;
+      case "change":
+        break;
+
+      case "flush":
+        break;
+
+      default:
+    }
+  }
+
+  function initGraph() {
+    var svg = d3.select("svg"),
+            width = +svg.attr("width"),
+            height = +svg.attr("height");
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    var simulation = d3.forceSimulation()
+            .force("link", d3.forceLink().id(function(d) { return d.id; }))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(width / 2, height / 2));
+
+
+    var link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(links)
+            .enter().append("line")
+            .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+    var node = svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("circle")
+            .data(nodes)
+            .enter().append("circle")
+            .attr("r", 5)
+            .attr("fill", function(d) { return color(d.group); })
+            .call(d3.drag()
+                  .on("start", dragstarted)
+                  .on("drag", dragged)
+                  .on("end", dragended));
+
+    node.append("title")
+              .text(function(d) { return d.id; });
+
+    simulation
+        .nodes(nodes)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(links);
+
+    function ticked() {
+      link
+          .attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+
+      node
+          .attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
+   }
+
+   function dragstarted(d) {
+     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+   }
+
+   function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+   }
+
+   function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+       d.fx = null;
+       d.fy = null;
+  }
+ }
+
+  BBabelWebV2= {};
+  BabelWebV2.connect = connect;
+  BabelWebV2.init = init;
+  BabelWebV2.initGraph = initGraph;
+
+  return BabelWebV2;
 }
-connect();
 
 
-/* ----------------------------    Message reçu ----------------------------    */
-function ConvertJSON(message) {
 
 
-}
+
+
+// var nodes = [
+//           {"id": "Myriel", "group": 1},
+//           {"id": "Napoleon", "group": 1},
+//           // {"id": "Mlle.Baptistine", "group": 1},
+//           // {"id": "Mme.Magloire", "group": 1},
+//           // {"id": "CountessdeLo", "group": 1},
+//           // {"id": "Geborand", "group": 1},
+//           // {"id": "Champtercier", "group": 1},
+//           // {"id": "Cravatte", "group": 1},
+//           // {"id": "Count", "group": 1},
+//           // {"id": "OldMan", "group": 1},
+//           // {"id": "Labarre", "group": 2},
+//           // {"id": "Valjean", "group": 2},
+//           // {"id": "Marguerite", "group": 3},
+//           // {"id": "Mme.deR", "group": 2},
+//           // {"id": "Isabeau", "group": 2},
+//           // {"id": "Gervais", "group": 2},
+//       ]
+//
+//       var links = [
+//           {"source": "Napoleon", "target": "Myriel", "value": 1},
+//         // {"source": "Mlle.Baptistine", "target": "Myriel", "value": 8},
+//         // {"source": "Mme.Magloire", "target": "Myriel", "value": 10},
+//         // {"source": "Mme.Magloire", "target": "Mlle.Baptistine", "value": 6},
+//         // {"source": "CountessdeLo", "target": "Myriel", "value": 1},
+//         // {"source": "Geborand", "target": "Myriel", "value": 1},
+//         // {"source": "Champtercier", "target": "Myriel", "value": 1},
+//         // {"source": "Cravatte", "target": "Myriel", "value": 1},
+//         // {"source": "Valjean", "target": "Labarre", "value": 1},
+//         // {"source": "Valjean", "target": "Mme.Magloire", "value": 3},
+//         // {"source": "Valjean", "target": "Mlle.Baptistine", "value": 3},
+//       ]
